efdf370c1178dbf06207e66f43af0b675abde406
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-03-25 21:38:34 +0100

    :sparkle: variables, event listeners

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import './scss/style.scss';
+
+const start = document.getElementById('start');
+const pepo = document.getElementById('pepo');
+const maxima = document.getElementById('maxima');
+const moschata = document.getElementById('moschata');
+
+start.addEventListener('click', printStart);
+pepo.addEventListener('click', printPepo);
+maxima.addEventListener('click', printMaxima);
+moschata.addEventListener('click', printMoschata);
+
+function printStart() {
+    console.log('start');
+}
+function printPepo() {
+    console.log('pepo');
+}
+function printMaxima() {
+    console.log('maxima');
+}
+function printMoschata() {
+    console.log('moschata');
+}
+
