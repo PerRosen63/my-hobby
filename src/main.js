cd8c3623d3ea3aa5f2314b55b96d8e5684be7427
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-03-27 23:37:17 +0100

    :speech_balloon: texts subpage 1

--- src/main.js
+++ src/main.js
@@ -60,7 +60,7 @@ printStart();
 function printPepo() {
     startTitle.innerText = 'Curcubita pepo';
     imgContainer.innerHTML = `<img width="150" height="150" src="src/images/Rondini 2.jpg"><img width="150" height="150" src="src/images/Zucchini 500x500zugeschnitten.jpg"><img width="150" height="150" src="src/images/lilpumkemon.jpg">`
-    textDivStart.innerHTML = `<p>Vestibulum fringilla pede sit amet augue. Etiam ut purus mattis mauris sodales aliquam. Duis leo. Nunc nec neque. Nam pretium turpis et arcu.</p>`;
+    textDivStart.innerHTML = `<p>Bland Curcubita pepo hittar vi både sommar- och vintervarianter. Sommarpumpor är t ex de avlånga sorterna som vi skördar späda och ofta kallar för zucchini. Dessa finns i olika färger och former, t ex runda eller tefatsformade. Några av sommarvarianterna går att skörda som vintervarianter, dvs de får växa färdigt. En sådan sort är den lilla rondinipumpan som är utsökt ungsbakad, hel som halvmogen och som mogen på vintern i halvor, då med kärnorna avlägsnade eftersom de hunnit bli sega.</p><p>Exempel på andra sorter är spagettipumpan vars fruktkött trådar sig vid tillagning och äts som spagetti samt nakenfröpumpan som odlas för sina frön. Det är från dessa pumpor vi får de pumpafrön som går att köpa i butiker. Fröna saknar "skal" och är därför ätfärdiga.</p><p>Fler Curcubita pepo-sorter är de lite lyxigare Acorn med nötig smak eller minipumpan Jack be Little, endast 10 cm i diameter. Dess motsats i storlek,Halloween-pumpan, kanske inte platsar på denna sida eftersom den i första hand används för dekoration. Vissa sorter går att använda, men kanske i första hand till soppa eller i bakverk.</p>`;
 }
 function printMaxima() {
     startTitle.innerText = 'Curcubita maxima';
